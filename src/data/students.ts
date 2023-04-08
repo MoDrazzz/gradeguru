e28600965850618e5df96a2671f9ae814635034b
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-04-08 08:13:12 +0200

    Add mocked data

---
+++ src/data/students.ts
@@ -0,0 +1,114 @@
+const Test: GradeType = {
+  id: 100,
+  name: "Test",
+  color: "#F87171",
+  weight: 3,
+  includeInAverage: true,
+};
+const ShortTest: GradeType = {
+  id: 101,
+  name: "Short Test",
+  color: "#FACC15",
+  weight: 2,
+  includeInAverage: true,
+};
+const Activity: GradeType = {
+  id: 102,
+  name: "Activity",
+  color: "#FB923C",
+  weight: 1,
+  includeInAverage: true,
+};
+const Homework: GradeType = {
+  id: 103,
+  name: "Homework",
+  color: "#4ADE80",
+  weight: 1,
+  includeInAverage: true,
+};
+
+export const gradeTypes: GradeType[] = [Test, ShortTest, Activity, Homework];
+
+export const students: Student[] = [
+  {
+    id: 1,
+    number: 1,
+    avatar: "https://i.pravatar.cc/150?img=1",
+    name: "Joanna Adamska",
+    group: 1,
+    grades: [
+      {
+        id: 10,
+        rating: 4,
+        type: Test,
+        comments: "Comment.",
+        dateAdded: 1680896700156,
+      },
+      {
+        id: 11,
+        rating: 5,
+        type: Test,
+        comments: "",
+        dateAdded: 1680897152227,
+      },
+      {
+        id: 12,
+        rating: 3,
+        type: ShortTest,
+        comments: "",
+        dateAdded: 1680897152227,
+      },
+      {
+        id: 13,
+        rating: 1,
+        type: Activity,
+        comments: "",
+        dateAdded: 1680897152227,
+      },
+      {
+        id: 14,
+        rating: 3,
+        type: Homework,
+        comments: "",
+        dateAdded: 1680897152227,
+      },
+    ],
+  },
+  {
+    id: 2,
+    group: 2,
+    number: 2,
+    avatar: "https://i.pravatar.cc/150?img=2",
+    name: "Jan Kowalski",
+    grades: [
+      {
+        id: 15,
+        rating: 5,
+        type: Homework,
+        comments: "",
+        dateAdded: 1680897152227,
+      },
+      {
+        id: 16,
+        rating: 5,
+        type: Test,
+        comments: "",
+        dateAdded: 1680897152227,
+      },
+      {
+        id: 17,
+        rating: 4,
+        type: Activity,
+        comments: "",
+        dateAdded: 1680897152227,
+      },
+      {
+        id: 18,
+        rating: 5,
+        type: Test,
+        comments: "",
+        dateAdded: 1680897152227,
+      },
+    ],
+  },
+];
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,3 +4,28 @@ type IconVariants =
   | "dashboard"
   | "arrowDown"
   | "arrowUp";
+
+interface GradeType {
+  id: number;
+  name: string;
+  color: string;
+  weight: number;
+  includeInAverage: boolean;
+}
+
+interface Grade {
+  id: number;
+  rating: number;
+  type: GradeType;
+  comments: string;
+  dateAdded: number;
+}
+
+interface Student {
+  id: number;
+  group: number;
+  number: number;
+  avatar: string;
+  name: string;
+  grades: Grade[];
+}
