467a179a7073178c9357538365842f916c56c10d
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-05-15 10:49:44 +0200

    Update next and change Error font weight

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function Error({ children }: Props) {
   return (
-    <span className="absolute left-0 top-full text-sm font-semibold text-red-600">
+    <span className="absolute left-0 top-full text-sm font-medium text-red-600">
       {children}
     </span>
   );
